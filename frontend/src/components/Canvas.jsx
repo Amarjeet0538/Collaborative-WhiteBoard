dde9f9a0ab0ea5d88723b592910cf23b24d586b6
amarjeet0538@ <amarjeet0538@gmail.com>
2026-02-07 23:58:50 +0530

    added header

---
+++ frontend/src/components/Canvas.jsx
@@ -0,0 +1,12 @@
+export default function Canvas() {
+  return (
+    <div className="p-2">
+      <canvas
+        id="canvas"
+        className="border-2 border-gray-300 rounded-lg w-full h-full"
+      >
+         Your browser does not support the HTML5 canvas element.
+      </canvas>
+    </div>
+  )
+}
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="p-2  bg-[#fef0f0] flex justify-between">
+      <span className='font-bold font-heading text-4xl'>WhiteBoard</span>
+
+    </div>
+  )
+}
