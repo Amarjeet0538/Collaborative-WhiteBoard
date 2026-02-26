9e8bdb358b98df23c2d38c5657cd5d5830a8ea6e
amarjeet0538@ <amarjeet0538@gmail.com>
2026-02-27 01:45:47 +0530

    added signup page and left side background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,10 @@ function App() {
 		<div className="p-2 font-body flex flex-col  h-screen gap-2 bg-gray-100">
 			<Header />
 			<div className="flex-1 min-h-0">
-				<WhiteBoard/>
+				<WhiteBoard />
 			</div>
 		</div>
-	)
+	);
 }
 
 export default App;
--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const AppRouter = createBrowserRouter([
 	{
@@ -10,6 +11,10 @@ const AppRouter = createBrowserRouter([
 	{
 		path: "/login",
 		element: <Login />,
+	},
+	{
+		path: "/signup",
+		element: <Signup />,
 	},
 ]);
 
