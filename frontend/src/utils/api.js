bfbf7fb36c66d83c06ba37c2c1ee331e386772c5
amarjeet0538@ <amarjeet0538@gmail.com>
2026-03-26 00:08:01 +0530

    added something

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,20 +1,20 @@
 // const BASE_URL = "http://localhost:5000/api";
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 function getToken() {
-  return localStorage.getItem("token");
+	return localStorage.getItem("token");
 }
 
 export async function apiFetch(endpoint, options = {}) {
-  const res = await fetch(`${BASE_URL}${endpoint}`, {
-    ...options,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getToken()}`,
-      ...options.headers,
-    },
-  });
-  const data = await res.json();
-  if (!res.ok) throw new Error(data.message || "Something went wrong");
-  return data;
+	const res = await fetch(`${BASE_URL}${endpoint}`, {
+		...options,
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${getToken()}`,
+			...options.headers,
+		},
+	});
+	const data = await res.json();
+	if (!res.ok) throw new Error(data.message || "Something went wrong");
+	return data;
 }
