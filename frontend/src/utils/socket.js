e3c578c9b564a1b7c474730d75b3c616cf075c7c
amarjeet0538@ <amarjeet0538@gmail.com>
2026-03-26 17:46:59 +0530

    Revert "fix: replace all hardcoded localhost URLs with env var"
    This reverts commit 2322e7a062f7bf921bba7035d587094ebf1fa87b.

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,8 +1,7 @@
 import { io } from "socket.io-client";
-const SOCKET_URL = import.meta.env.VITE_API_URL ?? "http://localhost:5000";
 
-const socket = io(SOCKET_URL, {
-	autoConnect: false,
+const socket = io("http://localhost:5000", {
+  autoConnect: false,
 });
 
 export default socket;
